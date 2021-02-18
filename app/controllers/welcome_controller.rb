46c0543b3b3df241799a24834ee2a460f0ab8ed3
Michael Rivera <soymrmike@gmail.com>
2021-02-17 19:26:47 -0800

    Simplified working example of two div updates via turbo streams.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,16 +6,8 @@ class WelcomeController < ApplicationController
 
   def history
     respond_to do |format|
-      format.turbo_stream do
-        # render 'welcome/test_stream'
-        render turbo_stream: turbo_stream.replace('inner-area', partial: 'history_inner_primary')
-      end
       format.html do
-        # response.content_type = 'text/vnd.turbo-stream.html'
-        # render turbo_stream: turbo_stream.replace('inner-area', partial: 'inner_push')
-        # render turbo_stream: turbo_stream.replace('inner-area', partial: 'history_inner_primary')
-        render partial: 'inner_push'
-        response.content_type = 'text/vnd.turbo-stream.html'
+        render partial: 'inner_push', content_type: 'text/vnd.turbo-stream.html'
       end
     end
   end
